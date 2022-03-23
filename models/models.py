8bab2f140cd1cb60c17b48f1600a089cbbb195b6
mariusapriutesa <72405372+mariusapriutesa@users.noreply.github.com>
2022-03-23 23:24:04 +0100

    cambiar cosas

--- models/models.py
+++ models/models.py
@@ -17,7 +17,6 @@
 #         for record in self:
 #             record.value2 = float(record.value) / 100
 
-from odoo import models, fields, api
 class equipo(models.Model):
     _name = 'jugador.equipo'
     _description = 'Define los atributos de un equipo'
@@ -26,75 +25,78 @@ class equipo(models.Model):
     nombreEquipo = fields.Char(string='Nombre equipo', required=True)
 
     #Relacion entre tablas
-    jugador_id = fields.One2many('jugador.jugador','equipo_id', string='Equipo')
+    empleado_id = fields.One2many('jugador.empleado','equipo_id', string='Equipo')
 
     def name_get(self):
-        listaEquipo= []
+        listaEquipos = []
         for equipo in self:
-            listaEquipo.append((equipo.id, equipo.nombreEquipo))
-        return listaEquipo
-        
-class jugador(models.Model):
-    _name = 'jugador.jugador'
-    _description = 'Define los atributos de un jugador'
+            listaEquipos.append((equipo.id, equipo.nombreEquipo))
+        return listaEquipos
+
+class empleado(models.Model):
+    _name = 'jugador.empleado'
+    _description = 'Define los atributos de un empleado'
 
     # Atributos
-    dniJugador = fields.Char(string='DNI', required=True)
-    nombreJugador = fields.Char(string='Nombre y apellidos', required=True)
+    dniEmpleado = fields.Char(string='DNI', required=True)
+    nombreEmpleado = fields.Char(string='Nombre y apellidos', required=True)
     fechaNacimiento = fields.Date(string='Fecha Nacimiento', required=True, default = fields.date.today())
-    direccionJugador = fields.Char(string='Direccon')
-    telefonoJugador = fields.Char(string='Telefono')
+    direccionEmpleado = fields.Char(string='Direccon')
+    telefonoEmpleado = fields.Char(string='Telefono')
     edad = fields.Integer('Edad', compute='_getEdad')
 
     #Relacion de tablas
     equipo_id = fields.Many2one('jugador.equipo', string='Equipo')
-    Torneos_ids = fields.Many2many('jugador.torneo', string='Torneos')
+    competicia_ids = fields.Many2many('jugador.competicia', string='Competicias')
 
 
     @api.depends('fechaNacimiento')
     def _getEdad(self):
         hoy = date.today()
-        for jugador in self:
-            jugador.edad = relativedelta(hoy, jugador.fechaNacimiento).years
+        for empleado in self:
+            empleado.edad = relativedelta(hoy, empleado.fechaNacimiento).years
 
     @api.constrains('fechaNacimiento')
     def _checkEdad(self):
-        for jugador in self:
-            if (jugador.edad < 18):
-                raise exceptions.ValidationError("El jugador no puede ser menor de edad")
+        for empleado in self:
+            if (empleado.edad < 18):
+                raise exceptions.ValidationError("El empleado no puede ser menor de edad")
     
-    @api.constrains('dniJugador')
+    @api.constrains('dniEmpleado')
     def _checkDNI(self):
-        for jugador in self:
-            if (len(jugador.dniJugador) > 9):
+        for empleado in self:
+            if (len(empleado.dniEmpleado) > 9):
                 raise exceptions.ValidationError("El DNI no puede tener mas 9 caracteres")
-            if (len(jugador.dniJugador) < 9):
+            if (len(empleado.dniEmpleado) < 9):
                 raise exceptions.ValidationError("El DNI no puede tener menos 9 caracteres")
 
-class torneo(models.Model):
-     _name = 'jugador.torneo'
-     _description = 'Define los atributos de un torneo'
+class competicia(models.Model):
+     _name = 'jugador.competicia'
+     _description = 'Define los atributos de un competicia'
 
      #Atributos
-     nombreTorneo = fields.Char(string='Nombre torneo', required=True)
-     tipoTorneo = fields.Selection(string='Tipo de torneo', selection=[('L','Local'),('I','Internacional')], help='Tipo de torneo al que se esta destinando' )
-     descripcionTorneo= fields.Text(string='Descripcion del torneo')
+     nombreCompeticia = fields.Char(string='Nombre competicia', required=True)
+     tipoCompeticia = fields.Selection(string='Tipo de competicia', selection=[('n','Nacional'),('i','Internacional')], help='Tipo de competicia al que se esta destinando' )
+     descripcionCompeticia = fields.Text(string='Descripcion del competicia')
      fechaInicio = fields.Date(string='Fecha de Inicio', required=True)
      fechaFin = fields.Date(string='Fecha de fin', required=True)
      #dias = fields.Integer(string='Dias')
      #Relacion entre tablas
-     jugador_id = fields.Many2many('jugador.jugador', string='Jugadores')
+     empleado_id = fields.Many2many('jugador.empleado', string='Empleados')
 
      @api.constrains('fechaFin')
      def _checkFechaFin(self):
-         for torneo in self:
-             if relativedelta(torneo.fechaInicio, torneo.fechaFin).days > 0:
+         for competicia in self:
+             if relativedelta(competicia.fechaInicio, competicia.fechaFin).days > 0:
                  raise exceptions.ValidationError("La fecha de fin no puede ser inferior a la fecha de inicio")
      
      @api.constrains('fechaInicio')
      def _checkFechaInicio(self):
          hoy = date.today()
-         for torneo in self:
-             diasCalculados = relativedelta(hoy, torneo.fechaInicio).days
+         for competicia in self:
+             diasCalculados = relativedelta(hoy, competicia.fechaInicio).days
              if ( diasCalculados > 0):
                  raise exceptions.ValidationError("La fecha no puede ser anterior a la fecha actual")
+
+     
+
