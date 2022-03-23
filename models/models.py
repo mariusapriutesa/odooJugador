2854a582e6e1b5d680df63ca8166d772989d0a6b
mariusapriutesa <72405372+mariusapriutesa@users.noreply.github.com>
2022-03-23 20:30:48 +0100

    he añadido la clase echipo

--- models/models.py
+++ models/models.py
@@ -16,3 +16,20 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+from odoo import models, fields, api
+class echipo(models.Model):
+    _name = 'jugador.equipo'
+    _description = 'Define los atributos de un equipo'
+
+    # Atributos
+    nombreEchipo = fields.Char(string='Nombre equipo', required=True)
+
+    #Relacion entre tablas
+    jugador_id = fields.One2many('jugador.jugador','equipo_id', string='Equipo')
+
+    def name_get(self):
+        listaEquipo= []
+        for equipo in self:
+            listaEquipo.append((equipo.id, equipo.nombreEquipo))
+        return listaEquipo
